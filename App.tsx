86d3a905885768edde2b9fc541a66a5174d5ea62
Byron Marcatoma <byronmarcatoma00@gmail.com>
2023-11-22 16:34:11 -0500

    initialise project & install vector icos

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text} from "react-native";
+import colors from './src/theme/colors';
+import font from './src/theme/fonts'
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const App = () =>{
+
+  return (
+    <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text style = {{ color:colors.primary, fontSize :font.size.xlg}}>
+        Hello world
+        <AntDesign name="stepforward" size ={25}/>
+      </Text>
+    </View>
+  )
+}
+
+export default App;
